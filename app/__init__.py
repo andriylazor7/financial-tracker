c50a3e8ff0c8e4f17a238d37c665c14532932243
AndriyLazor <236327@student.uek.krakow.pl>
2025-03-24 17:18:16 +0100

    Registration and Login functionality implemented

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "SECRET_KEY"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+def create_app():
+    app = Flask(__name__, template_folder="templates_", static_folder="static_")
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///finance_tracker.db"
+    app.config["SECRET_KEY"] = "your_secret_key_here"
 
-from app import routes, models
+    db.init_app(app)
+    login_manager.init_app(app)
 
+    from app.routes import auth_bp  
+    app.register_blueprint(auth_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models import User
+    return User.query.get(int(user_id))
