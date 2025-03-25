30d9c314aa6486eb6933032cfcd3f5117deb9ca7
AndriyLazor <236327@student.uek.krakow.pl>
2025-03-25 09:55:09 +0100

    Added class Category

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from app import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from app import db
 
 class User(db.Model, UserMixin):
   id = db.Column(db.Integer, primary_key=True)
@@ -13,4 +15,12 @@ class User(db.Model, UserMixin):
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
   
+class Category(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), nullable=False, unique=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  
+  def __repr__(self):
+    return f'<Category {self.name}>'
+
   
